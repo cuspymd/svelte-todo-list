fcb8f51cf9b3a919fd1aaf6b5be7f560f821f9bb
Myoungdo Park <cuspymd@gmail.com>
2022-01-01 02:38:44 +0000

    Edit todo

--- src/routes/todos/_api.ts
+++ src/routes/todos/_api.ts
@@ -1,7 +1,7 @@
 import type { Request } from "@sveltejs/kit"
 let todos: Todo[] = [];
 
-export const api = (request: Request, todo?: Todo) => {
+export const api = (request: Request, data?: Record<string, unknown>) => {
     let body = {};
     let status = 500;
 
@@ -11,14 +11,22 @@ export const api = (request: Request, todo?: Todo) => {
             status = 200;
             break;
         case "POST":
-            todos.push(todo);
-            body = todo;
+            todos.push(data as Todo);
+            body = data;
             status = 201;
             break;
         case "DELETE":
             todos = todos.filter(todo => todo.uid !== request.params.uid);
             status = 200;
             break;
+        case "PATCH":
+            todos = todos.map(todo => {
+                if (todo.uid === request.params.uid) {
+                    todo.text = data.text as string
+                }
+                return todo;
+            })
+            status = 200;
     }
 
     if (request.method.toUpperCase() !== "GET") {
