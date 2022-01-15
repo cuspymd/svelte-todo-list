c5a1810d335982a497e2e2067f168c45ec8674f8
Oracle Public Cloud User <opc@instance-cuspymd.subnet01070036.vcn01070036.oraclevcn.com>
2022-01-15 17:51:52 +0000

    Set up local database

--- shell.nix
+++ shell.nix
@@ -4,8 +4,19 @@ pkgs.mkShell {
   buildInputs = [
     pkgs.emacs
     pkgs.git
+    pkgs.nodejs-16_x
     pkgs.nodePackages.npm
+    pkgs.docker
+    pkgs.docker-compose
   ];
 
+  shellHook =
+    ''
+    echo "Starting dev database"
+    docker-compose -f prisma/docker-compose.yml up -d
+    trap "docker-compose -f prisma/docker-compose.yml down" EXIT
+    '';
+
   TERM = "xterm-256color";
+  DATABASE_URL = "postgres://postgres@localhost:6543/todos";
 }
