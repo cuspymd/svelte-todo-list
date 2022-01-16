02d8d1a094dd6b21f14bbcf17fab8346d05141e6
Oracle Public Cloud User <opc@instance-cuspymd.subnet01070036.vcn01070036.oraclevcn.com>
2022-01-16 07:59:13 +0000

    Merge login UI

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,8 +1,34 @@
-import type { Handle } from "@sveltejs/kit";
+import { parse } from "cookie";
+import { getSession as getSessionFromApi } from "./routes/api/_db";
+
+import type { Handle, GetSession } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ request, resolve }) => {
     if (request.query.has("_method")) {
         request.method = request.query.get("_method").toUpperCase();
     }
+
+    const cookies = parse(request.headers.cookie || '');
+
+    if (cookies.session_id) {
+        const session = await getSessionFromApi(cookies.session_id);
+        if (session) {
+            request.locals.user = { email: session.email };
+            return resolve(request);
+        }
+
+    }
+
+    request.locals.user = null;
     return await resolve(request);
+}
+
+export const getSession: GetSession = request => {
+    return request?.locals?.user
+    ? {
+        user: {
+            email: request.locals.user.email
+        }
+    }
+    : {};
 }
