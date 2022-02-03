79b1d856cd360da404b3fe868e553226dbfdfb06
Myoungdo Park <cuspymd@gmail.com>
2022-02-03 22:30:48 +0900

    Fix prisma error on nixos

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/df123677560db3b0db7c19d71981b11091fbeaf6.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/a7ecde854aee5c4c7cd6177f54a99d2c1ff28a31.tar.gz") {} }:
 
 pkgs.mkShell {
   buildInputs = [
@@ -8,13 +8,20 @@ pkgs.mkShell {
     pkgs.nodePackages.npm
     pkgs.docker
     pkgs.docker-compose
+    pkgs.nodePackages.prisma
   ];
 
-  shellHook =
+  shellHook = with pkgs;
     ''
     echo "Starting dev database"
     docker-compose -f prisma/docker-compose.yml up -d
     trap "docker-compose -f prisma/docker-compose.yml down" EXIT
+    export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
+    export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+    export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+    export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+    export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+    npm install
     '';
 
   TERM = "xterm-256color";
